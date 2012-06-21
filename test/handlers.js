6e2d06f2677814022648b47db9683136ef762de6
Thiago de Arruda <tpadilha84@gmail.com>
2012-06-21 11:35:47 -0300

    Validating types in a soft way now

--- test/handlers.js
+++ test/handlers.js
@@ -11,25 +11,47 @@ var assert = require('assert')
 * 2 - a set of incorrect values
 */
 suite('Handlers:', function() {
-  var key
+  var fnMsg = 'Stub' 
+  , key
+  , CustomModel = Model.extend({}, {}, {
+      fn: fnMsg
+    })
   , fixtures = {
-    stringType: [{p:{type: String}}, ['str','str2'], [1,4.3,/abc/,{}]],
-    numberType: [{p:{type: Number}}, [1,5,10e10], ['str',/abc/,{},[]]],
-    arrayType: [{p:{type: Array}}, [[1],[5,10e10]], [1,'str',/abc/,{},]],
-    objectType: [{p:{type: Object}}, [1,'as',true,[1,2],{n:'a'}],[]],
-    required: [{p:{required: true}}, ['req1'], [null, undefined, '']],
+    booleanType:[{p:{type: 'boolean'}}, [true,'false'], ['t',1,4.3,{}]],
+    // anything can go in a string type
+    stringType:[{p:{type: 'string'}},[7,'str2'], []],
+    numberType:[{p:{type: 'number'}},[1.7,5,'12e-1'],['str',/abc/,{},[]]],
+    integerType:[{p:{type: 'integer'}},[1,'10',4],['s',4.3,10e-5]],
+    arrayType: [{p:{type: 'array'}},[[1],['a','b']],[1,'str',/abc/,{},]],
+    dateType: [{p:{type: 'date'}}, [
+        2332234423, //number of seconds since epoch 
+        new Date(),
+        'October 13, 1975 11:13:00', 
+        '07/08/2000',
+        '2012-06-20', 
+        '2012-06-20 07:33:00'
+        ], ['invalid']],
+    required: [{p:{required: true}},['req1'], [null, undefined, '']],
     minlength: [{p:{minlength: 4}}, ['abcd'], ['abc']],
     maxlength: [{p:{maxlength: 6}}, ['abcdef'],['abcdefg']],
     min: [{p:{min: 11}}, [11], [10]],
     max: [{p:{max: 6}}, [6], [7]],
     email: [{p:{email: true}}, ['abc@def.com'], ['not an email']],
     url: [{p:{url: true}}, ['http://www.google.com'], ['www.google.com']],
-    date: [{p:{date: true}}, ['October 13, 1975 11:13:00'], ['invalid']],
-    dateISO: [{p:{dateISO: true}}, ['2012-06-20'], ['20/06/2012']],
     number: [{p:{number: true}}, ['23.2','232'], ['abc','2332.2.2']],
     digits: [{p:{digits: true}}, ['2463672'], ['43563d35']],
     // from http://www.ihwy.com/labs/jquery-validate-credit-card-extension.aspx
-    creditcard: [{p:{creditcard: true}}, ['370000000000002'], ['5424180832']]
+    creditcard: [{p:{creditcard: true}}, ['370000000000002'], ['5424180832']],
+    fn: [{
+      p: {
+        fn: function(value, target, property) {
+          // custom validation
+          if (value !== 'correct')
+            return fnMsg;
+        }
+      }
+    }, ['correct'], ['aadfag', 124, /abc/]]
+
   };
 
   for (key in fixtures) {
@@ -46,10 +68,11 @@ suite('Handlers:', function() {
       test("'" + key + "'" + ' with incorrect values', function() {
         for (var i = 0; i < incorrect.length; i++) {
           var result = model.validate({p:incorrect[i]});
-          assert.equal(1, result.p.length);
+          assert.notEqual(null, result);
+          assert.equal(result.p.length, 1);
           assert.equal(result.p[0], model.messages[handlerKey]);
         };
       });
-    })(Model.extend(fixtures[key][0]), fixtures[key][1], fixtures[key][2]);
+    })(CustomModel.extend(fixtures[key][0]), fixtures[key][1], fixtures[key][2]);
   }
 });
